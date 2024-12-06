25789b765adefd30ba59223c7a289ae4f4fd5a48
Sean Demeritt <seandemeritt13@gmail.com>
2024-12-06 17:44:45 -0500

    v2

---
+++ src/routes/body.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { writable } from 'svelte/store';
+	import { defaultData, type Word, type Data } from "$lib";
+	export let formFields = writable<Data>(defaultData);
+
+	const addNewField = () => {
+		formFields.update(data => {
+			return {
+				...data,
+				words: [...(data.words || []), { oldWord: "", newWord: "" }]
+			};
+		});
+		window.scrollTo({
+			top: document.body.scrollHeight,
+			behavior: 'smooth'
+		});
+	};
+
+	const deleteField = (index: number) => {
+		formFields.update(data => {
+			return {
+				...data,
+				words: data.words.filter((_, i) => i !== index)
+			};
+		});
+	};
+</script>
+
+{#each $formFields.words as field, i}
+	<div class="flex space-x-2 px-2 pt-1">
+		<input
+			class="input input-sm input-bordered input-primary text-sm w-64"
+			type="text"
+			placeholder="text to transform"
+			bind:value={field.oldWord}
+		/>
+		<input
+			class="input input-sm input-bordered input-primary text-sm w-64"
+			type="text"
+			placeholder="text to display"
+			bind:value={field.newWord}
+		/>
+		<div>
+			<button
+				class="btn btn-sm btn-error"
+				type="button"
+				on:click|preventDefault={() => deleteField(i)}>
+				x
+			</button>
+		</div>
+	</div>
+{/each}
+<div class="p-2">
+	<button
+		class="btn btn-sm bg-success"
+		type="button"
+		on:click|preventDefault={addNewField}>
+		+
+	</button>
+</div>
---
+++ src/routes/header.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { type Data, defaultData, type Word } from "$lib";
+	import { writable } from "svelte/store";
+	import ThemeDropdown from "./themeDropdown.svelte";
+	export let formFields = writable<Data>(defaultData);
+
+	const downloadFormData = () => {
+		const filteredWords = $formFields.words.filter(
+			(item: Word) => item.oldWord.trim() !== '' || item.newWord.trim() !== '');
+		const csvRows = [];
+		csvRows.push(['Old Word', 'New Word'].join(','));
+		filteredWords.forEach((item: Word) => {
+			csvRows.push([item.oldWord, item.newWord].join(','));
+		});
+		const csvContent = csvRows.join('\n');
+		const csvBlob = new Blob([csvContent], { type: 'text/csv' });
+		const url = URL.createObjectURL(csvBlob);
+		const currentDate = new Date();
+		const formattedDate = currentDate.toISOString().split('T')[0];
+		const formattedTime = currentDate.toLocaleTimeString().replace(/:/g, '-').replace(/\//g, '-');
+		const filename = `text-transform-${formattedDate}-${formattedTime}.csv`;
+		console.log(filename)
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = filename;
+		a.click();
+		URL.revokeObjectURL(url);
+	};
+
+
+	const uploadFormData = (event: Event) => {
+		const fileInput = event.target as HTMLInputElement;
+		const file = fileInput.files?.[0];
+		if (!file) return;
+
+		const reader = new FileReader();
+		reader.onload = (event) => {
+			try {
+				const csvContent = event.target?.result as string;
+				const rows = csvContent.split('\n');
+				 let words = rows.slice(1).map((row) => {
+					const [oldWord, newWord] = row.split(',');
+					return { oldWord: oldWord.trim(), newWord: newWord.trim() };
+				});
+					words = words.filter(
+					word => word.oldWord !== "Old Word" && word.newWord !== "New Word"
+				);
+				formFields.update(currFields => {
+					const filteredWords = currFields.words.filter(word => word.oldWord?.trim() !== "");
+					return {
+						...currFields,
+						words: [
+							...filteredWords,
+							...words,
+							{ oldWord: "", newWord: "" }
+						]
+					};
+				});
+			} catch (error) {
+				console.error('Error processing CSV data:', error);
+			}
+		};
+		reader.readAsText(file);
+		fileInput.value = "";
+	};
+
+	const onUploadClick = () => {
+		document.getElementById('fileInput')?.click();
+	};
+</script>
+
+<div>
+	<h1 class="text-4xl text-center text-primary font-semibold py-4">
+		TransformText
+	</h1>
+	<div class="flex flex-col mx-2">
+		<input
+			type="file"
+			id="fileInput"
+			accept=".csv"
+			style="display: none;"
+			on:change={uploadFormData}
+		/>
+		<div class="flex flex-row justify-between">
+			<button class="flex btn btn-sm btn-success items-center w-[16ch]" on:click={onUploadClick}>
+				<i class="fas fa-upload mr-1"></i>
+				Upload
+			</button>
+			<ThemeDropdown />
+			<button class="flex btn btn-sm btn-success items-center w-[16ch]" on:click={downloadFormData}>
+				<i class="fas fa-download mr-1"></i>
+				Download
+			</button>
+		</div>
+	</div>
+	<div class="divider divider-primary opacity-50 m-0 mb-1 "></div>
+</div>
